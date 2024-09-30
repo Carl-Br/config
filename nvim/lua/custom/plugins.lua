117128b1bc9f676a0de23c4250f755d9f5bca79d
Carl <62182341+Carl-Br@users.noreply.github.com>
2024-09-30 15:29:13 +0200

    updated harpoon to harpoon v2

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -84,8 +84,22 @@ local plugins = {
       vim.cmd [[silent! GoInstallDeps]]
     end,
   },
-  {
-    "ThePrimeagen/harpoon",
-  }
+{
+    "theprimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup()
+    end,
+    keys = {
+      { "<leader>A", function() require("harpoon"):list():add() end, desc = "add file to harpoon", },
+      { "<leader>a", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon menu", },
+      { "<leader>1", function() require("harpoon"):list():select(1) end, desc = " harpoon to File 1", },
+      { "<leader>2", function() require("harpoon"):list():select(2) end, desc = " harpoon to File 2", },
+      { "<leader>3", function() require("harpoon"):list():select(3) end, desc = " harpoon to File 3", },
+      { "<leader>4", function() require("harpoon"):list():select(4) end, desc = " harpoon to File 4", },
+      { "<leader>5", function() require("harpoon"):list():select(5) end, desc = " harpoon to File 5", },
+    },
+  },
 }
 return plugins
--- nvim/lua/custom/remaps.lua
+++ nvim/lua/custom/remaps.lua
@@ -1,10 +1,6 @@
 -- general
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n','<s-g>','<s-g>zz')
 vim.keymap.set('n', '<leader>gd', '<leader>gdzz')
 
--- harpoon
-vim.keymap.set('n', '<leader>ha', '<cmd>lua require("harpoon.mark").add_file()<CR>',{desc="add File to Harpoon"})
-vim.keymap.set('n', '<leader>hm', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>',{desc="toggle Harpoon Menu"})
-vim.keymap.set('n', '<leader>j', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>',{desc="jump to Harpoon File 1"})
-vim.keymap.set('n', '<leader>k', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>',{desc="jump to Harpoon File 2"})
