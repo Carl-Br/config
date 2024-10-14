74fb08e36f2be43c531aa6f8fc4135277b65fae1
Carl <62182341+Carl-Br@users.noreply.github.com>
2024-10-14 21:50:21 +0200

    remap to center after to definition

--- nvim/lua/custom/remaps.lua
+++ nvim/lua/custom/remaps.lua
@@ -2,5 +2,7 @@
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n','<s-g>','<s-g>zz')
-vim.keymap.set('n', '<leader>gd', '<leader>gdzz')
+vim.keymap.set('n', 'gd', function()
+    vim.cmd('normal! gdzz')
+end)
 
