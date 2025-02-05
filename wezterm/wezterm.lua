9cd8c7e90dc9fa84cbfd39872bd1c58d6caede9b
Carl <62182341+Carl-Br@users.noreply.github.com>
2025-02-05 19:08:48 +0100

    removed special remap which conflicted with my naviagtion mappings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -23,28 +23,5 @@ config.window_background_opacity = 0.9
 -- other wise it will be used as a meta key
 config.send_composed_key_when_left_alt_is_pressed = true
 
-config.keys = {
-  -- Nur remappen, wenn nicht in Vim
-  -- da sonst nicht auto completion funktioniert 
-  -- sondern in die nächste Zeile gesprungen wird
-  {
-    key = 'j',
-    mods = 'CTRL',
-    action = wezterm.action_callback(function(win, pane)
-      local current_prog = pane:get_foreground_process_name()
-
-      -- Nur remap, wenn das aktuelle Programm nicht Vim ist
-      if not current_prog:match("vim") then
-        win:perform_action(wezterm.action.SendKey { key = 'RightArrow' }, pane)
-      else
-        -- Wenn in Vim, standardmäßiges Verhalten
-        win:perform_action(wezterm.action.SendKey { key = 'f', mods = 'CTRL' }, pane)
-      end
-    end),
-  },
-}
-
-
-
 return config
 
