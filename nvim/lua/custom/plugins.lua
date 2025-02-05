4be9cc7e0d55942211daccf29a744b675c48a119
Carl <62182341+Carl-Br@users.noreply.github.com>
2025-02-05 19:08:02 +0100

    changed theme and removed naviagation in nvchad, which conflicted with a the new tmux plugin vim-tmux-navigator

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -30,7 +30,7 @@ local plugins = {
      event = "VeryLazy",
     config = function()
       -- Set Copilot keymap to use <C-F> instead of <Tab>
-      vim.keymap.set('i', '<C-f>', 'copilot#Accept("\\<CR>")', {
+      vim.keymap.set('i', '<C>-f>', 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = false
       })
@@ -39,7 +39,21 @@ local plugins = {
   },
   {
     "christoomey/vim-tmux-navigator",
-    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   },
   -- NVIM DAP SETTINGS
   {
